a947752e7c445f88f81632822093e5542230eec8
DakshSemwal-zs <dakshsemwal35@gmail.com>
2023-11-05 17:38:40 +0530

    File Upload

--- build-logic/convention/src/main/kotlin/com/dakshsemwal/AndroidBuildConfig.kt
+++ build-logic/convention/src/main/kotlin/com/dakshsemwal/AndroidBuildConfig.kt
@@ -1,8 +1,6 @@
 package com.dakshsemwal
 
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+const val PUBLISHING_GROUP = "io.github.dakshsemwal"
 
 object AndroidBuildConfig {
     const val applicationId = "com.dakshsemwal.retrofitfileuploadkit"
@@ -10,15 +8,10 @@ object AndroidBuildConfig {
     const val minSdkVersion = 26
     const val targetSdkVersion = 33
 
-    // yyyymmdd followed by build number on the given day
-    // eg: second build on 15th march 2032 will be 2032031502
-    private val currentDate: String = SimpleDateFormat("yyyyMMdd", Locale.US).format(Date())
-
     // TODO update on every release
     private const val buildNumber = "0" // increment this for each build on the same day
-    private const val patch = "01" // increment this for each build on the same day
     val versionCode =
-        (currentDate + patch).toInt() // e.g. 2023050201 for May 2nd, 2023 (first build of the day)
-    const val versionName: String = "1.5.${buildNumber}"
+        1
+    const val versionName: String = "1.0.${buildNumber}"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
